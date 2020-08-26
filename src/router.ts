4a9e6d3f55ef2cbf31b7f9087abba71e54557866
Gaocarri <gaocarri@163.com>
2020-08-26 23:08:09 +0800

    添加doc默认路由

--- src/router.ts
+++ src/router.ts
@@ -6,6 +6,7 @@ import SwitchDemo from './components/SwitchDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
+import DocDemo from './components/DocDemo.vue'
 
 const history = createWebHashHistory()
 export const router = createRouter({
@@ -19,6 +20,7 @@ export const router = createRouter({
             path: '/doc',
             component: Doc,
             children: [
+                { path: '', component: DocDemo },
                 { path: 'switch', component: SwitchDemo },
                 { path: 'button', component: ButtonDemo },
                 { path: 'dialog', component: DialogDemo },
