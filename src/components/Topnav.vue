ca07ff8430f0c9fab34a07f3e3c306680c06ad7a
Gaocarri <gaocarri@163.com>
2020-08-26 20:39:50 +0800

    让topNav响应式

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,46 +1,67 @@
 <template>
-  <div class="topnav">
+<div class="topnav">
     <div class="logo" @click="toggleMenu">LOGO</div>
     <ul class="menu">
-      <li>菜单1</li>
-      <li>菜单2</li>
+        <li>菜单1</li>
+        <li>菜单2</li>
     </ul>
-  </div>
+    <span class="toggleMenu"></span>
+</div>
 </template>
 
 <script lang="ts">
-import { inject, Ref } from "vue";
+import {
+    inject,
+    Ref
+} from "vue";
 export default {
-  setup() {
-    const menuVisible = inject<Ref<boolean>>("menuVisible");
-    const toggleMenu = () => {
-      menuVisible.value = !menuVisible.value;
-    };
-    return {
-      toggleMenu,
-    };
-  },
+    setup() {
+        const menuVisible = inject < Ref < boolean >> ("menuVisible");
+        const toggleMenu = () => {
+            menuVisible.value = !menuVisible.value;
+        };
+        return {
+            toggleMenu,
+        };
+    },
 };
 </script>
 
 <style lang="scss" scoped>
 .topnav {
-  background: pink;
-  display: flex;
-  padding: 16px;
-  position: relative;
-  z-index: 10;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-  }
-  > .menu {
+    background: pink;
     display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
+    padding: 16px;
+    position: relative;
+    z-index: 10;
+    justify-content: center;
+    align-items: center;
+
+    >.logo {
+        max-width: 6em;
+        margin-right: auto;
     }
-  }
+
+    >.menu {
+        display: flex;
+        white-space: nowrap;
+        flex-wrap: nowrap;
+
+        >li {
+            margin: 0 1em;
+        }
+    }
+
+    >.toggleMenu {}
+
+    @media (max-width: 500px) {
+        >.menu {
+            display: none;
+        }
+
+        >.logo {
+            margin: 0 auto;
+        }
+    }
 }
 </style>
