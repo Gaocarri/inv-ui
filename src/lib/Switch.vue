b2eecb5ded88af34452bd5b4e2fec7c64a623cb1
Gaocarri <gaocarri@163.com>
2020-08-27 20:12:17 +0800

    switch切换按钮

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,11 +1,25 @@
 <template>
-<button>
+<button @click="toggle" :class="{checked:checked}">
     <span></span>
 </button>
 </template>
 
 <script>
-export default {};
+import {
+    ref
+} from "vue";
+export default {
+    setup() {
+        const checked = ref(false);
+        const toggle = () => {
+            checked.value = !checked.value;
+        };
+        return {
+            checked,
+            toggle,
+        };
+    },
+};
 </script>
 
 <style lang="scss">
@@ -16,9 +30,10 @@ button {
     height: $h1;
     width: $h1 * 2;
     border: none;
-    background-color: blue;
+    background-color: grey;
     border-radius: $h1/2;
     position: relative;
+    transition: background-color 0.3s ease-in-out;
 
     >span {
         height: $h2;
@@ -28,6 +43,21 @@ button {
         position: absolute;
         top: 2px;
         left: 2px;
+        transition: left 0.3s ease-in-out;
+    }
+
+    &.checked {
+        background-color: blue;
+        border: none;
+
+        >span {
+            left: calc(100% - 2px - #{$h2});
+        }
+    }
+
+    // 解决点击button有圈圈的问题
+    &:focus {
+        outline: none;
     }
 }
 </style>
